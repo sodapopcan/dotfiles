51fefd762a133fb15e7bded57c58605a37127a4e
Andrew Haust <andrewwhhaust@gmail.com>
2018-01-20 20:10:31 -0500

    ZSH: Fix battery she-bang

--- sh/battery.sh
+++ sh/battery.sh
@@ -1,4 +1,4 @@
-#! /usr/local/bin/zsh
+#! /usr/bin/env zsh
 
 bat_curr=`ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.2f"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}'`
 
