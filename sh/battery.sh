180973d24e98fabc468351750f678746f8c1aa11
Andrew Haust <andrewwhhaust@gmail.com>
2018-02-20 09:55:11 -0500

    SH: Break long line

--- sh/battery.sh
+++ sh/battery.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/env zsh
 
-bat_curr=`ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.2f"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}'`
+bat_curr=`ioreg -n AppleSmartBattery -r | awk '\
+  $1~/Capacity/{c[$1]=$3} END{OFMT="%.2f"; max=c["\"MaxCapacity\""];\
+  print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}\
+'`
 
 bat_glyphs=(\
 '▏  ' \
