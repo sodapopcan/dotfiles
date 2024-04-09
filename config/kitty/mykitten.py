b3b94d9bb2fdfd4c8b14a19cccfa058b36c4cf3d
Andrew Haust <andrew@hau.st>
2024-04-09 19:19:02 -0400

    Fix new tab layout

--- config/kitty/mykitten.py
+++ config/kitty/mykitten.py
@@ -4,17 +4,21 @@ from kitty.boss import Boss
 def main(args: List[str]) -> str:
     # this is the main entry point of the kitten, it will be executed in
     # the overlay window when the kitten is launched
-    directory = input('New tab: ')
+    name = input('New tab: ')
     # whatever this function returns will be available in the
     # handle_result() function
-    return directory
+    return name
 
-def handle_result(args: List[str], directory: str, target_window_id: int, boss: Boss) -> None:
+def handle_result(args: List[str], name: str, target_window_id: int, boss: Boss) -> None:
     window = boss.window_id_map.get(target_window_id)
     if window is not None:
         boss.call_remote_control(window, ('action', f'new_tab'))
-        boss.call_remote_control(window, ('send-text', 'j ' + directory + '\r'))
-        boss.call_remote_control(window, ('action', f'set_tab_title', directory))
+        boss.call_remote_control(window, ('send-text', 'j ' + name + '\r'))
+        boss.call_remote_control(window, ('action', f'set_tab_title', name))
         boss.call_remote_control(window, ('launch', f'--location=hsplit', '--cwd=current'))
+        boss.call_remote_control(window, ('send-text', 'j ' + name + '\r'))
         boss.call_remote_control(window, ('launch', f'--location=vsplit', '--cwd=current'))
-        boss.call_remote_control(window, ('focus-window', 0))
+        boss.call_remote_control(window, ('send-text', 'j ' + name + '\r'))
+        boss.call_remote_control(window, ('action', f'previous_window'))
+        boss.call_remote_control(window, ('action', f'previous_window'))
+        boss.call_remote_control(window, ('action', f'resize_window taller 20'))
