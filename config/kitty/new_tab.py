4d274893a90cc2d75c7cdb1c66b74601f87c0d2f
Andrew Haust <andrew@hau.st>
2024-04-27 22:45:14 -0400

    Change new_tab kitten again

--- config/kitty/new_tab.py
+++ config/kitty/new_tab.py
@@ -11,28 +11,34 @@ def handle_result(args, location_and_maybe_name, target_window_id, boss):
     pieces = location_and_maybe_name.split(':')
     location = pieces[0]
 
-    if location == "":
-        location = os.getcwd()
-    else:
-        location = subprocess.check_output(["autojump", location]).rstrip().decode("utf-8")
-
     if len(pieces) > 1:
-        name = pieces[1]
+        title = pieces[1]
     else:
-        name = os.path.basename(location)
+        title = "~"
 
     window = boss.window_id_map.get(target_window_id)
 
     if window is not None:
-        boss.call_remote_control(window, ('launch', '--type=tab', f'--cwd={location}'))
-        boss.call_remote_control(window, ('action', 'set_tab_title', name))
+        boss.call_remote_control(window, ('action', 'new_tab'))
+        boss.call_remote_control(window, ('action', 'set_tab_title', title))
+        _set_win(boss, window, location)
 
         if num_windows > 1:
-            boss.call_remote_control(window, ('launch', '--location=hsplit', f'--cwd={location}'))
+            # boss.call_remote_control(window, ('action', 'new_window_with_cwd'))
+            boss.call_remote_control(window, ('launch', '--type=window', '--location=hsplit'))
+            _set_win(boss, window, location)
 
             if num_windows > 2:
-                boss.call_remote_control(window, ('launch', '--location=vsplit', f'--cwd={location}'))
+                boss.call_remote_control(window, ('launch', '--type=window', '--location=vsplit'))
+                _set_win(boss, window, location)
                 boss.call_remote_control(window, ('action', 'previous_window'))
 
             boss.call_remote_control(window, ('action', 'previous_window'))
             boss.call_remote_control(window, ('action', 'resize_window taller 20'))
+
+def _set_win(boss, window, location):
+    if location != "":
+        boss.call_remote_control(window, ('send-text', f'j {location}\n'))
+
+    boss.call_remote_control(window, ('send-key', 'ctrl+l'))
+
